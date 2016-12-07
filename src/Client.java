3dbb24455c699e555c26c2b698a786be75556f6f
Rich Cherngchaosil <richer_po@hotmail.com>
2016-12-06 21:06:48 -0800

    Socket Server

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,55 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class Client {
 
-	public static void main(String[] args){
 
+	private int 		mPort;
+	private String 		mServerName;
+
+	public Client(String pServerName, int port){
+		mPort 		= port;
+		mServerName = pServerName;
+	}
+
+	public void start(){
+		Socket client = null;
+		try{
+			System.out.println("Connecting on port " + mPort);
+			client = new Socket(mServerName, mPort);
+
+			System.out.println("Just connected to " + client.getRemoteSocketAddress());
+			OutputStream outToServer = client.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+
+			out.writeUTF("Hello from " + client.getLocalSocketAddress());
+			InputStream inFromServer = client.getInputStream();
+			DataInputStream in = new DataInputStream(inFromServer);
+
+			System.out.println("Server: " + in.readUTF());
+			while(true){
+
+			}
+
+		} catch(IOException e){
+			e.printStackTrace();
+		} finally{
+			try {
+				System.out.println("closing...");
+				client.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		Client client = new Client("localhost", 4524);
+		client.start();
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,60 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class Server {
 
-	public static void main(String[] args){
+	private ServerSocket serverSocket;
 
+	public Server(int port) throws IOException{
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(10000);
+	}
+
+	public void start(){
+		Socket server = null;
+		while(true){
+			System.out.println("Server listening on port " + serverSocket.getLocalPort());
+			try{
+				server = serverSocket.accept();
+				System.out.println("Just coinnected to " + server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(server.getInputStream());
+
+				System.out.println(in.readUTF());
+				DataOutputStream out = new DataOutputStream(server.getOutputStream());
+				out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
+
+				while(true){
+
+				}
+			} catch(SocketTimeoutException s){
+				System.out.println("Socket timed out");
+			} catch (IOException e){
+				e.printStackTrace();
+				break;
+			} finally{
+				try {
+					server.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+	public static void main(String[] args){
+		Server server = null;
+		try {
+			server = new Server(4524);
+			server.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
